e2b8918a740fe8331534c82851bdf5ac769ba7ef
Ahmed Taiwo <t_whizzy101@yahoo.com>
2021-10-05 11:58:45 +0100

    New edits

--- src/main/java/KafkaDemoApp/ClassConfigs/Processor/ProcessorClass.java
+++ src/main/java/KafkaDemoApp/ClassConfigs/Processor/ProcessorClass.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.function.Function;
 
 @Configuration
